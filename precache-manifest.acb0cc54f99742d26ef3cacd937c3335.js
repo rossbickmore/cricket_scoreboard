f2ccd2f2f93afae1247edbce179028bedafab3d0
ROSS BICKMORE <rossbickmore@gmail.com>
2019-10-27 12:13:52 +0000

    Updates

---
+++ precache-manifest.acb0cc54f99742d26ef3cacd937c3335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cf8f5f62dd8ba4182e2887b365add3",
+    "url": "/cricket_scoreboard/index.html"
+  },
+  {
+    "revision": "283ecc16e1a9bbe01f6e",
+    "url": "/cricket_scoreboard/static/css/main.d0257479.chunk.css"
+  },
+  {
+    "revision": "1251e8d5e573a603bcb8",
+    "url": "/cricket_scoreboard/static/js/2.7bbe077a.chunk.js"
+  },
+  {
+    "revision": "283ecc16e1a9bbe01f6e",
+    "url": "/cricket_scoreboard/static/js/main.de74e304.chunk.js"
+  },
+  {
+    "revision": "c404a61d4cad04418cec",
+    "url": "/cricket_scoreboard/static/js/runtime~main.9b91f903.js"
+  }
+]);
